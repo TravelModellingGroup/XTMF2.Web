2b8d884f3ca249d866d0bd7cde4a4714bfd90edb
Brendan Reilly <brendan.reilly@utoronto.ca>
2019-10-12 11:17:52 -0400

    Fixed compilation issue

--- src/XTMF2.Web.Components/Areas/Modal/ModalBase.cs
+++ src/XTMF2.Web.Components/Areas/Modal/ModalBase.cs
@@ -1,66 +1,57 @@
-
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace XTMF2.Web.Components
-{
+namespace XTMF2.Web.Components {
 
-    public class ModalBase : ComponentBase
-    {
+	public class ModalBase : ComponentBase {
 
-        [Inject]
-        public IJSRuntime JsRuntime { get; set; }
+		[Inject]
+		public IJSRuntime JsRuntime { get; set; }
 
-        protected ElementReference modalElement;
+		protected ElementReference modalElement;
 
-        [Parameter]
-        public RenderFragment Content { get; set; }
+		[Parameter]
+		public RenderFragment Content { get; set; }
 
+		[Parameter]
+		public RenderFragment Title { get; set; }
 
-        [Parameter]
-        public RenderFragment Title { get; set; }
+		[Parameter]
+		public string ModalName { get; set; }
 
-        [Parameter]
-        public string ModalName { get; set; }
+		[Parameter]
+		public string ConfirmText { get; set; } = "Confirm";
 
-        [Parameter]
-        public string ConfirmText { get; set; } = "Confirm";
+		protected bool IsShow { get; set; } = false;
 
-        protected bool IsShow { get; set; } = false;
+		protected string ShowClass => IsShow ? "show" : "";
 
-        protected string ShowClass => IsShow ? "show" : "";
+		[Parameter]
+		public EventCallback<EventArgs> OnConfirm { get; set; }
 
-        [Parameter]
-        private EventCallback<EventArgs > OnConfirm { get; set; }
+		[Parameter]
+		public EventCallback<EventArgs> OnCancel { get; set; }
 
-        [Parameter]
-        private EventCallback<EventArgs > OnCancel { get; set; }
+		public async void Show () {
+			await JsRuntime.InvokeAsync<object> ("xtmf2WebComponents.Modal.showModal", ModalName);
+			this.IsShow = true;
+		}
 
+		public async void Hide () {
+			await JsRuntime.InvokeAsync<object> ("xtmf2WebComponents.Modal.showModal", ModalName);
+			this.IsShow = true;
+		}
 
-        public async void Show()
-        {
-            await JsRuntime.InvokeAsync<object>("xtmf2WebComponents.Modal.showModal", ModalName);
-            this.IsShow = true;
-        }
+		public void Confirm (EventArgs e) {
+			this.IsShow = false;
+			OnConfirm.InvokeAsync (e);
+		}
 
-        public async void Hide()
-        {
-            await JsRuntime.InvokeAsync<object>("xtmf2WebComponents.Modal.showModal", ModalName);
-            this.IsShow = true;
-        }
-
-        public void Confirm(EventArgs  e)
-        {
-            this.IsShow = false;
-           OnConfirm.InvokeAsync(e);
-        }
-
-        public void Cancel(EventArgs  e)
-        {
-            this.IsShow = false;
-            OnCancel.InvokeAsync(e);
-        }
-    }
+		public void Cancel (EventArgs e) {
+			this.IsShow = false;
+			OnCancel.InvokeAsync (e);
+		}
+	}
 }
