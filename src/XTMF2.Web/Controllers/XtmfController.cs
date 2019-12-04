33538de7b9a2d9c22bd92503b40eeb9b86b8ebbd
Brendan Reilly <brendan.reilly@utoronto.ca>
2019-12-04 11:18:34 -0500

    Added API controllers to deal with requests from client

---
+++ src/XTMF2.Web/Controllers/XtmfController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XTMF2.Web.Controllers
+{
+    /// <summary>
+    /// API controller for methods and functions related to XTMF.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XtmfController : ControllerBase
+    {
+    }
+}
