54c6094e94ed7fbf22bb1f1a9bb6a893059c5644
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-01-31 17:07:37 -0500

    Added ModelSystemControllerUnitTests base class

---
+++ tests/XTMF2.Web.UnitTests/Controllers/ModelSystemControllerUnitTests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using XTMF2.Web.Data.Models;
+using XTMF2.Web.Server.Controllers;
+using XTMF2.Web.Server.Profiles;
+using XTMF2.Web.Server.Session;
+using Xunit;
+
+namespace XTMF2.Web.UnitTests.Controllers {
+    public class ModelSystemControllerUnitTests {
+
+        private IMapper _mapper;
+        private XTMFRuntime _runtime;
+        private ILogger<ProjectController> _logger;
+        private ModelSystemController _controller;
+        private UserSession _userSession;
+
+        public ModelSystemControllerUnitTests() {
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<ProjectProfile>();
+            });
+            _mapper = config.CreateMapper();
+            _runtime = TestHelper.CreateTestContext();
+            _logger = Mock.Of<ILogger<ProjectController>>();
+            _controller = new ModelSystemController(_runtime, _logger, _mapper);
+            _userSession = new UserSession(_runtime.UserController.GetUserByName("TempUser"));
+        }
+
+    }
+}
