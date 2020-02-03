9382b8fa02c0dff37265d7ac0922fefe7d564635
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-02-03 10:15:01 -0500

    Comments updated

--- src/XTMF2.Web.Server/Profiles/ModelSystemProfile.cs
+++ src/XTMF2.Web.Server/Profiles/ModelSystemProfile.cs
@@ -3,6 +3,9 @@ using XTMF2.Web.Data.Models;
 
 namespace XTMF2.Web.Server.Profiles {
 
+    /// <summary>
+    /// AutoMapper profile
+    /// </summary>
     public class ModelSystemProfile : Profile {
 
         public ModelSystemProfile () {
