e472f26a1493c8706c0b259e18c6b5158067d061
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-02-16 10:07:37 -0500

    Added new editing tracker

---
+++ src/XTMF2.Web.Server/Session/ModelSystemEditingTracker.cs
@@ -0,0 +1,57 @@
+//     Copyright 2017-2020 University of Toronto
+// 
+//     This file is part of XTMF2.
+// 
+//     XTMF2 is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     XTMF2 is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using XTMF2.Editing;
+using XTMF2.Web.Data.Models.Editing;
+
+namespace XTMF2.Web.Server.Session
+{
+
+    public class ModelSystemEditingTracker
+    {
+        /// <summary>
+        /// Tracks GUID to model system editing references
+        /// </summary>
+        /// <typeparam name="Guid"></typeparam>
+        /// <typeparam name="object"></typeparam>
+        /// <returns></returns>
+        public Dictionary<Guid, object> ModelSystemEditingObjectReferenceMap { get; }= new Dictionary<Guid, object>();
+
+        /// <summary>
+        /// Tracks the underlying model system to editing model references
+        /// </summary>
+        /// <typeparam name="object"></typeparam>
+        /// <typeparam name="object"></typeparam>
+        /// <returns></returns>
+        public Dictionary<object, object> ModelSystemObjectRefrenceMap { get; }= new Dictionary<object, object>();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelSystem"></param>
+        public ModelSystemEditingTracker(ModelSystemEditingModel modelSystem)
+        {
+            ModelSystem = modelSystem;
+        }
+        public ModelSystemEditingModel ModelSystem { get; private set; }
+    }
+
+}
